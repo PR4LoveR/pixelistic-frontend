e8d147470cc9c16e78159660a93f3a5f675c7f90
user_roman <user_roman@localhost.localdomain>
2020-05-14 17:34:28 +0300

    final version

--- src/const/node-server-config.js
+++ src/const/node-server-config.js
@@ -1,3 +1,3 @@
-export const port = 3000;
-export const host = 'http://18.156.188.230';
-export const awsImage = 'https://s3.eu-central-1.amazonaws.com/pixelistic';
+export const port = 443;
+export const host = 'https://18.156.188.230';
+export const awsImage = 'https://s3.eu-central-1.amazonaws.com/pixelistic-js';
