403837ead5830f033c511355991fdcf9cb9ea871
Roman Plyta <44133157+PR4LoveR@users.noreply.github.com>
2020-05-22 17:38:44 +0300

    Update node-server-config.js

--- src/const/node-server-config.js
+++ src/const/node-server-config.js
@@ -1,3 +1,6 @@
-export const port = 443;
-export const host = 'https://18.156.109.74';
+const hostname = process.env.HOST;
+const port = process.env.PORT; 
+
+export const port = ${PORT};
+export const host = 'https://${HOST}';
 export const awsImage = 'https://s3.eu-central-1.amazonaws.com/pixelistic-js';
